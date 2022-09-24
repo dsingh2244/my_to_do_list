8b1f4ffc4146c7e40286eaef249e628a6f01c91c
dsingh2244 <dsinght4@gmail.com>
2022-09-24 23:28:13 +0530

    cleanup button added

--- todoscr.js
+++ todoscr.js
@@ -2,16 +2,32 @@ let inpnewtask = $('#inpnewtask');
 let btnadd = $('#btnadd');
 let btnclear = $('#btnclear');
 let ultask = $('#ultask');
+let btncleanup = $('#btncleanup');
 
-btnadd.click(function() {
+function addli() {
     let listitem = $('<li>', {
         'class': 'list-group-item',
         text: inpnewtask.val()
     })
+    listitem.click(function() {
+        listitem.toggleClass("done");
+    })
     ultask.append(listitem);
     inpnewtask.val('');
-});
+}
+
+function cleanup() {
+    $('#ultask .done').remove();
+}
+
+inpnewtask.keypress(function(e) {
+    if (e.which == 13) {
+        addli();
+    }
+})
+btnadd.click(addli);
+btncleanup.click(cleanup);
 
 btnclear.click(function() {
-
+    inpnewtask.val('');
 });
