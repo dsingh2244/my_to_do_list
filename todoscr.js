98b3e2219b5e8603d46acf26265e2abc65bca05c
dsingh2244 <dsinght4@gmail.com>
2022-09-25 00:55:10 +0530

    completed it was fun

--- todoscr.js
+++ todoscr.js
@@ -3,6 +3,9 @@ let btnadd = $('#btnadd');
 let btnclear = $('#btnclear');
 let ultask = $('#ultask');
 let btncleanup = $('#btncleanup');
+let btnsort = $('#btnsort');
+let uldone = $('#ultask .done');
+let cnt = 0;
 
 function addli() {
     let listitem = $('<li>', {
@@ -11,13 +14,29 @@ function addli() {
     })
     listitem.click(function() {
         listitem.toggleClass("done");
+        cnt++;
+        togglebtnclear();
     })
     ultask.append(listitem);
     inpnewtask.val('');
+    togglebtnclear();
 }
 
 function cleanup() {
     $('#ultask .done').remove();
+    cnt = 0;
+    togglebtnclear();
+}
+
+function sort() {
+    $('#ultask .done').appendTo(ultask);
+}
+
+function togglebtnclear() {
+    btnclear.prop('disabled', inpnewtask.val() == '')
+    btnadd.prop('disabled', inpnewtask.val() == '')
+    btncleanup.prop('disabled', cnt < 1)
+    btnsort.prop('disabled', cnt < 1)
 }
 
 inpnewtask.keypress(function(e) {
@@ -25,9 +44,13 @@ inpnewtask.keypress(function(e) {
         addli();
     }
 })
+
+inpnewtask.on('input', togglebtnclear)
 btnadd.click(addli);
 btncleanup.click(cleanup);
+btnsort.click(sort);
 
 btnclear.click(function() {
     inpnewtask.val('');
+    togglebtnclear();
 });
